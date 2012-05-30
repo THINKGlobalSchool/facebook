70640215bd0425e15a7de0d4e4e311b8eec74a7e
Jeff Tilson <jrtilson@gmail.com>
2012-05-30 17:29:11 -0400

    Basic plugin files, admin area and facebook-php-sdk libary

---
+++ languages/en.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Facebook Integration English Language Translation
+ * 
+ * @package Facebook Integration
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010 - 2012
+ * @link http://www.thinkglobalschool.com/
+ * 
+ */
+
+$english = array(
+	// Generic
+	
+	// Page titles 
+
+	// Labels
+	'facebook:label:appid' => 'Facebook App ID',
+	'facebook:label:appsecret' => 'Facebook App Secret',
+	'facebook:label:facebooksettings' => 'Facebook Settings',
+
+	// Messages
+
+	// River
+
+	// Notifications
+
+	// Other content
+);
+
+add_translation('en',$english);
