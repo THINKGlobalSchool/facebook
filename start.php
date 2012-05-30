a603ff7be757bebc1049de40de3b6937d4159b02
Jeff Tilson <jrtilson@gmail.com>
2012-05-30 16:34:13 -0400

    Initial Commit

---
+++ start.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Facebook Integration start.php
+ * 
+ * @package Facebook Integration
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010 - 2012
+ * @link http://www.thinkglobalschool.com/
+ * 
+ */
+
+elgg_register_event_handler('init','system','facebook_init');
+
+function facebook_init() {
+}
